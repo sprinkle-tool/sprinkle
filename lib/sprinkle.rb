cdf1d7a557f8e8d88dccc0df5a14b608a13eb44d
Marcus Crafter <crafterm@redartisan.com>
2008-04-14 19:53:56 +1000

    Automatically load extensions to existing classes or those that active support will miss

--- lib/sprinkle.rb
+++ lib/sprinkle.rb
@@ -2,11 +2,15 @@ require 'rubygems'
 require 'logger'
 require 'active_support'
 
+# Use active supports auto load mechanism
 Dependencies.load_paths << File.dirname(__FILE__)
 
 # Configure active support to log auto-loading of dependencies
 #Dependencies::RAILS_DEFAULT_LOGGER = Logger.new($stdout)
 #Dependencies.log_activity = true
+
+# Load up extensions to existing classes
+Dir[File.dirname(__FILE__) + 'sprinkle/extensions/*.rb'].each { |e| require e }
 
 # Define a global logger thats available everywhere
 class Object
