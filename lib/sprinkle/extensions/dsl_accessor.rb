06d684b4047ef65020f7af62e42e7d80cc95902c
Marcus Crafter <crafterm@redartisan.com>
2008-04-14 20:03:17 +1000

    Add DSL accessor extension

---
+++ lib/sprinkle/extensions/dsl_accessor.rb
@@ -0,0 +1,15 @@
+class Module
+  def dsl_accessor(*symbols)
+    symbols.each do |sym|
+      class_eval %{
+        def #{sym}(*val)
+          if val.empty?
+            @#{sym}
+          else
+            @#{sym} = val.size == 1 ? val[0] : val
+          end
+        end
+      }
+    end
+  end
+end
