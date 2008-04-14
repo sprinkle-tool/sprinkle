f2bb4e7df51abb0c49b3fd005263180e1dd39961
Marcus Crafter <crafterm@redartisan.com>
2008-04-14 22:46:38 +1000

    Updated arbitrary options to work directly on the class its included into

--- lib/sprinkle/extensions/arbitrary_options.rb
+++ lib/sprinkle/extensions/arbitrary_options.rb
@@ -1,16 +1,10 @@
 module ArbitraryOptions
-      
   def self.included(base)
-    base.extend ClassMethods
+    base.alias_method_chain :method_missing, :arbitrary_options
   end
   
-  module ClassMethods
-    
-    def method_missing_with_arbitrary_options(sym, *args, &block)
-      self.class.dsl_accessor sym
-      send(sym, *args, &block)
-    end
-        
-    alias_method_chain :method_missing, :arbitrary_options
+  def method_missing_with_arbitrary_options(sym, *args, &block)
+    self.class.dsl_accessor sym
+    send(sym, *args, &block)
   end
 end
