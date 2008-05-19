152c9ff1954d760cf6785a749b26a6ea8802f1c8
Marcus Crafter <crafterm@redartisan.com>
2008-05-19 23:34:03 +1000

    Merge older deploy_spec into sprinkle_spec, implement DSL loader tests

--- spec/sprinkle/deploy_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe 'Deploy loader' do
-  
-  it 'should automatically extend Object to support deployment DSL keywords'
-  it 'should require the correct actor implementation'
-
-end
--- spec/sprinkle/sprinkle_spec.rb
+++ spec/sprinkle/sprinkle_spec.rb
@@ -1,6 +1,12 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Sprinkle do 
+
+  it 'should automatically extend Object to support package, policy and deployment DSL keywords' do 
+    %w( package policy deployment ).each do |keyword|
+      Object.should respond_to(keyword.to_sym)
+    end
+  end
   
   it 'should default to production mode' do 
     Sprinkle::OPTIONS[:testing].should be_false
