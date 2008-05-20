4e23a03a5434f6b0a20738cbca809eaad8c0c07a
Marcus Crafter <crafterm@redartisan.com>
2008-05-20 14:55:41 +1000

    Add virtual package specs

--- lib/sprinkle/policy.rb
+++ lib/sprinkle/policy.rb
@@ -3,55 +3,55 @@ require 'highline/import'
 module Sprinkle
   module Policy
     POLICIES = []
-  
+
     def policy(name, options = {}, &block)
       p = Policy.new(name, options, &block)
       POLICIES << p
       p
     end
-  
+
     class Policy
       attr_reader :name, :packages
-    
+
       def initialize(name, metadata = {}, &block)
         raise 'No name provided' unless name
         raise 'No roles provided' unless metadata[:roles]
-        
+
         @name = name
         @roles = metadata[:roles]
         @packages = []
         self.instance_eval(&block)
       end
-    
+
       def requires(package, options = {})
         @packages << package
       end
-    
+
       def to_s; name; end
-    
+
       def process(deployment)
         all = []
-      
+
         puts "Package hierarchy for policy #{@name}\n\n"
-      
+
         @packages.each do |p|
           puts "Policy #{@name} requires package #{p}"
-        
+
           package = Sprinkle::Package::PACKAGES[p]
           package = select_package(p, package) if package.is_a? Array # handle virtual package selection
-        
+
           tree = package.tree do |parent, child, depth|
             depth.times { print "\t" }
             puts "Package #{parent.name} requires #{child.name}"
           end
-        
+
           puts
-        
+
           all << tree
         end
-      
+
         all = all.flatten.uniq
-      
+
         puts
         puts "Normalized installation order for all packages: #{all.collect(&:name).join(', ')}"
 
@@ -59,7 +59,7 @@ module Sprinkle
           package.process(deployment, @roles)
         end
       end
-    
+
       def select_package(name, packages)
         if packages.size <= 1
           package = packages.first
@@ -68,11 +68,11 @@ module Sprinkle
             menu.prompt = "Multiple choices exist for virtual package #{name}"
             menu.choices *packages.collect(&:to_s)
           end
-          package = Sprinkle::Packages::PACKAGES[package]
+          package = Sprinkle::Package::PACKAGES[package]
         end
-      
+
         puts "Selecting #{package.to_s} for virtual package #{name}"
-      
+
         package
       end
     end
--- spec/sprinkle/policy_spec.rb
+++ spec/sprinkle/policy_spec.rb
@@ -2,102 +2,112 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Sprinkle::Policy do
   include Sprinkle::Policy
-  
-  before do 
+
+  before do
     @name = 'a policy'
   end
-  
+
   describe Sprinkle::Policy, 'when created' do
-    
-    it 'should be invalid without a name' do 
+
+    it 'should be invalid without a name' do
       lambda { policy nil }.should raise_error
     end
-    
-    it 'should be invalid without role definitions' do 
+
+    it 'should be invalid without role definitions' do
       lambda { policy @name do; end }.should raise_error
       lambda { policy @name, :roles => :app do; end }.should_not raise_error
     end
-    
-    it 'should optionally accept package dependencies' do 
+
+    it 'should optionally accept package dependencies' do
       p = policy @name, :roles => :app do; end
       p.should respond_to(:requires)
       p.requires :appserver
       p.packages.should == [ :appserver ]
     end
-    
-    it 'should optionally accept package dependencies with versions' do 
+
+    it 'should optionally accept package dependencies with versions' do
       p = policy @name, :roles => :app do; end
       p.requires :appserver, :version => 2
       p.packages.should == [ :appserver ]
       pending 'requires version checking implementation'
     end
-    
-    it 'should add itself to the global policy list' do 
+
+    it 'should add itself to the global policy list' do
       sz = Sprinkle::Policy::POLICIES.size
       p = policy @name, :roles => :app do; end
       Sprinkle::Policy::POLICIES.size.should == sz + 1
       Sprinkle::Policy::POLICIES.last.should == p
     end
-    
+
   end
-  
+
   describe Sprinkle::Policy, 'with packages' do
     include Sprinkle::Package
-    
-    before do 
+
+    before do
       @deployment = mock(Sprinkle::Deployment)
+      Sprinkle::Package::PACKAGES.clear # reset full package list before each spec is run
 
       @a = package :a do; requires :b; requires :c; end
-      @b = package :b do; end
-      @c = package :c do; end
-      @d = package :d do; end
-      
+      @b = package :b, :provides => :xyz do; end
+      @c = package :c, :provides => :abc do; end
+      @d = package :d, :provides => :abc do; end
+
       @policy = policy :test, :roles => :app do; requires :a; end
+      $terminal.stub!(:choose).and_return(:c) # stub out highline asking questions
     end
 
     describe Sprinkle::Policy, 'when applying' do
       include Sprinkle::Package
-      
-      it 'should determine the packages to install via the hierarchy dependency tree of each package in the policy' do 
+
+      it 'should determine the packages to install via the hierarchy dependency tree of each package in the policy' do
         @a.should_receive(:process).and_return
         @b.should_receive(:process).and_return
         @c.should_receive(:process).and_return
         @d.should_not_receive(:process)
       end
-      
-      it 'should normalize (ie remove duplicates from) the installation order of all packages including dependencies' do 
+
+      it 'should normalize (ie remove duplicates from) the installation order of all packages including dependencies' do
         @e = package :e do; requires :b; end
         @policy.requires :e
-        
+
         @a.should_receive(:process).once.and_return
         @b.should_receive(:process).once.and_return
         @c.should_receive(:process).once.and_return
         @d.should_not_receive(:process)
         @e.should_receive(:process).once.and_return
       end
-      
+
       it 'should process each normalized package in reverse dependency order'
-      
-      after do 
-        @policy.process(@deployment)
-      end
     end
 
     describe Sprinkle::Policy, 'containing package dependencies with versions' do
-      
+
       it 'should be invalid if the specified package does not exist'
       it 'should ignore any packages of the same name that have other versions'
       it 'should select the correct package version when applying'
-      
+
     end
 
     describe Sprinkle::Policy, 'containing virtual packages' do
-      
-      it 'should automatically select a concrete package implementation for a virtual one when there exists only one possible selection'
-      it 'should ask the user for the concrete package implementation to use for a virtual one when more than one possible choice exists' 
-      
+
+      it 'should automatically select a concrete package implementation for a virtual one when there exists only one possible selection' do
+        @policy = policy :virtual, :roles => :app do; requires :xyz; end
+        Sprinkle::Package::PACKAGES[:xyz].should == [ @b ]
+      end
+
+      it 'should ask the user for the concrete package implementation to use for a virtual one when more than one possible choice exists' do
+        @policy = policy :virtual, :roles => :app do; requires :abc; end
+        Sprinkle::Package::PACKAGES[:abc].should include(@c)
+        Sprinkle::Package::PACKAGES[:abc].should include(@d)
+        $terminal.should_receive(:choose).and_return(:c)
+      end
+
     end
-    
+
+    after do
+      @policy.process(@deployment)
+    end
   end
-  
+
 end
