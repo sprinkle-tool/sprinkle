c3461788925a86e2570498e97b5b280f888079a4
Marcus Crafter <crafterm@redartisan.com>
2008-05-20 18:37:28 +1000

    Implemented gem installer specs

--- lib/sprinkle/installers/gem.rb
+++ lib/sprinkle/installers/gem.rb
@@ -1,21 +1,22 @@
 module Sprinkle
   module Installers
     class Gem < Installer
+      attr_accessor :gem
+
       def initialize(parent, gem, &block)
         super parent, &block
         @gem = gem
       end
-      
+
       protected
-      
+
         # rubygems 0.9.5+ installs dependencies by default, and does platform selection
-        # REVISIT: assume yes? how do we handle multiple versions? install version specified in the package if available
-        
+
         def install_sequence
           unless version
-            ["gem install #{@gem}"]
+            "gem install #{@gem}"
           else
-            ["gem install #{@gem} --version #{version}"]
+            "gem install #{@gem} --version '#{version}'"
           end
         end
     end
--- spec/sprinkle/installers/gem_spec.rb
+++ spec/sprinkle/installers/gem_spec.rb
@@ -1,18 +1,59 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe Sprinkle::Installers::Gem, 'installer' do
-  
-  it 'should accept a single package to install' do
-    #@installer = Sprinkle::Installers::Gem.new('rake')
-    #@installer.package.should == 'rake'
+describe Sprinkle::Installers::Gem do
+
+  before do
+    @gem = 'rails'
+    @version = '2.0.2'
   end
-  
-end
 
-describe Sprinkle::Installers::Gem, 'during installation' do
-  
-  it 'should invoke the gem installer for all specified package'
-  it 'should install a specific version if defined'
-  
-end
+  def create_gem(gem, version = nil, &block)
+    @package = mock(Sprinkle::Package, :name => gem, :version => version)
+    Sprinkle::Installers::Gem.new(@package, gem, &block)
+  end
 
+  describe 'when created' do
+
+    before do
+      @installer = create_gem @gem, @version
+    end
+
+    it 'should accept a single package to install' do
+      @installer.gem.should == @gem
+    end
+
+    it 'should optionally store a version of the gem to install' do
+      @installer.version.should == '2.0.2'
+    end
+
+  end
+
+  describe 'during installation' do
+
+    describe 'without a version' do
+
+      before do
+        @installer = create_gem @gem
+      end
+
+      it 'should invoke the gem installer for all specified package' do
+        @installer.send(:install_sequence).should == "gem install #{@gem}"
+      end
+
+    end
+
+    describe 'with a specific version' do
+
+      before do
+        @installer = create_gem @gem, @version
+      end
+
+      it 'should install a specific version if defined' do
+        @installer.send(:install_sequence).should == "gem install #{@gem} --version '#{@version}'"
+      end
+
+    end
+
+  end
+
+end
