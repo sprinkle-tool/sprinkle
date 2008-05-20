08e32ac8feeac6b89c8ac369237c5046d4bd3e3b
Marcus Crafter <crafterm@redartisan.com>
2008-05-20 19:58:46 +1000

    Implemented source based installer command sequence specs (pre/post command specs pending)

--- lib/sprinkle/installers/source.rb
+++ lib/sprinkle/installers/source.rb
@@ -1,82 +1,87 @@
 module Sprinkle
   module Installers
-    class Source < Installer      
+    class Source < Installer
+      attr_accessor :source, :pre, :post
+
       def initialize(parent, source, &block)
         @pre = {}; @post = {}
         @source = source
         super parent, &block
       end
-      
+
       def pre(stage, *commmands)
-        @pre[stage] ||= []
-        @pre[stage] += commands
+        (@pre[stage] ||= []) << commands
       end
-    
+
       def post(stage, *commands)
-        @post[stage] ||= []
-        @post[stage] += commands
+        (@post[stage] ||= []) << commands
       end
-      
+
       protected
 
         def install_sequence
           prepare + download + extract + configure + build + install
         end
-        
+
         %w( prepare download extract configure build install ).each do |stage|
           define_method stage do
             pre_commands(stage.to_sym) + self.send("#{stage}_commands") + post_commands(stage.to_sym)
           end
         end
-        
+
         def prepare_commands
-          [ "mkdir -p #{@options[:archives]}",
+          raise 'No installation area defined' unless @options[:prefix]
+          raise 'No build area defined' unless @options[:builds]
+          raise 'No source download area defined' unless @options[:archives]
+
+          [ "mkdir -p #{@options[:prefix]}",
             "mkdir -p #{@options[:builds]}",
-            "mkdir -p #{@options[:prefix]}" ]
+            "mkdir -p #{@options[:archives]}" ]
         end
-        
+
         def download_commands
           [ "wget -cq --directory-prefix='#{@options[:archives]}' #{@source}" ]
         end
-        
+
         def extract_commands
           [ "bash -c 'cd #{@options[:builds]} && #{extract_command} #{@options[:archives]}/#{archive_name}'" ]
         end
-                
+
         def configure_commands
           return [] if custom_install?
-          
+
           command = "bash -c 'cd #{build_dir} && ./configure --silent --prefix #{@options[:prefix]} "
 
-          # REVISIT: should support disable/with/without as well
-          if @options[:enable]
-            extras = @options[:enable].collect { |option| "--enable-#{option}" }
-            command << extras.join(' ')
-          end
-          
+          extras = {
+            :enable  => '--enable', :disable => '--disable',
+            :with    => '--with',   :without => '--without'
+          }
+
+          extras.inject(command) { |m, (k, v)| m << create_options(k, v) if options[k]; m }
+
           [ command << " > #{@package.name}-configure.log 2>&1'" ]
         end
-        
+
         def build_commands
           return [] if custom_install?
           [ "bash -c 'cd #{build_dir} && make > #{@package.name}-build.log 2>&1'" ]
         end
-        
+
         def install_commands
           return custom_install_commands if custom_install?
           [ "bash -c 'cd #{build_dir} && make install > #{@package.name}-install.log 2>&1'" ]
         end
-        
+
       private
-        
+
         def pre_commands(stage)
           dress @pre[stage] || [], :pre
         end
-        
+
         def post_commands(stage)
           dress @post[stage] || [], :post
         end
-        
+
         def dress(commands, stage)
           commands.collect { |command| "bash -c 'cd #{build_dir} && #{command} >> #{@package.name}-#{stage}.log 2>&1'" }
         end
@@ -89,7 +94,11 @@ module Sprinkle
         def custom_install_commands
           dress @options[:custom_install], :install
         end
-        
+
+        def create_options(key, prefix)
+          @options[key].inject(' ') { |m, option| m << "#{prefix}-#{option} "; m }
+        end
+
         def extract_command
           case @source
           when /(tar.gz)|(tgz)$/
@@ -110,7 +119,7 @@ module Sprinkle
           raise "Unable to determine archive name for source: #{source}, please update code knowledge" unless name
           name
         end
-        
+
         def build_dir
           "#{@options[:builds]}/#{base_dir}"
         end
--- spec/sprinkle/installers/source_spec.rb
+++ spec/sprinkle/installers/source_spec.rb
@@ -1,101 +1,215 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe Sprinkle::Installers::Source, 'when created' do
-  
-  it 'should accept a source archive name to install'
-  
+describe Sprinkle::Installers::Source do
+  include Sprinkle::Deployment
+
+  before do
+    @source = 'ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6-p111.tar.gz'
+
+    @deployment = deployment do
+      delivery :capistrano
+      source do
+        prefix   '/usr'
+        archives '/usr/archives'
+        builds   '/usr/builds'
+      end
+    end
+
+    @installer = create_source @source do
+      prefix   '/usr/local'
+      archives '/usr/local/archives'
+      builds   '/usr/local/builds'
+
+      enable %w( headers ssl deflate so )
+      disable %w( cache proxy rewrite )
+
+      with %w( debug extras )
+      without %w( fancyisms )
+    end
+
+    @installer.defaults(@deployment)
+  end
+
+  def create_source(source, version = nil, &block)
+    @package = mock(Sprinkle::Package, :name => 'package', :version => version)
+    Sprinkle::Installers::Source.new(@package, source, &block)
+  end
+
+  describe 'when created' do
+
+    it 'should accept a source archive name to install' do
+      @installer.source.should == @source
+    end
+
+  end
+
+  describe 'before installation' do
+
+    before do
+      @settings = { :prefix => '/usr/local', :archives => '/usr/local/tmp', :builds => '/usr/local/stage' }
+    end
+
+    it 'should fail if no installation area has been specified' do
+      @settings.delete(:prefix)
+    end
+
+    it 'should fail if no build area has been specified' do
+      @settings.delete(:builds)
+    end
+
+    it 'should fail if no source download area has been specified' do
+      @settings.delete(:archives)
+    end
+
+    after do
+      @settings.each { |k, v| @installer.send k, v }
+      lambda { @installer.install_sequence }.should raise_error
+    end
+
+  end
+
+  describe  'customized configuration' do
+
+    it 'should support specification of "enable" options' do
+      @installer.enable.should == %w( headers ssl deflate so )
+    end
+
+    it 'should support specification of "disable" options' do
+      @installer.disable.should == %w( cache proxy rewrite )
+    end
+
+    it 'should support specification of "with" options' do
+      @installer.with.should == %w( debug extras )
+    end
+
+    it 'should support specification of "without" options' do
+      @installer.without.should == %w( fancyisms )
+    end
+
+    it 'should support customized build area' do
+      @installer.prefix.should == '/usr/local'
+    end
+
+    it 'should support customized source area' do
+      @installer.archives.should == '/usr/local/archives'
+    end
+
+    it 'should support customized install area' do
+      @installer.builds.should == '/usr/local/builds'
+    end
+
+  end
+
+  describe 'during gnu source archive style installation' do
+
+    it 'should prepare the build, installation and source archives area' do
+      @installer.should_receive(:prepare).and_return(
+        [
+         'mkdir -p /usr/local',
+         'mkdir -p /usr/local/builds',
+         'mkdir -p /usr/local/archives'
+        ]
+      )
+
+    end
+
+    it 'should download the source archive' do
+      @installer.should_receive(:download).and_return(
+        [
+         "wget -cq --directory-prefix='/usr/local/archives' #{@source}"
+        ]
+      )
+    end
+
+    it 'should extract the source archive' do
+      @installer.should_receive(:extract).and_return(
+        [
+         "bash -c 'cd /usr/local/builds && tar xzf /usr/local/archives/ruby-1.8.6-p111.tar.gz"
+        ]
+      )
+    end
+
+    it 'should configure the source'
+
+    it 'should build the source' do
+      @installer.should_receive(:build).and_return(
+        [
+         "bash -c 'cd /usr/local/builds && make > #{@package.name}-build.log 2>&1'"
+        ]
+      )
+    end
+
+    it 'should install the source' do
+      @installer.should_receive(:install).and_return(
+        [
+         "bash -c 'cd /usr/local/builds && make install > #{@package.name}-install.log 2>&1'"
+        ]
+      )
+    end
+
+    after do
+      @installer.send :install_sequence
+    end
+
+  end
+
+  describe 'during customized installation' do
+
+    it 'should prepare the build area'
+    it 'should prepare the installation area'
+    it 'should prepare the source download area'
+    it 'should download the source archive'
+    it 'should extract the source archive'
+    it 'should not configure the source automatically'
+    it 'should not build the source automatically'
+    it 'should install the source using a custom installation command'
+
+  end
+
+  describe 'customized installer commands' do
+
+    it 'should be run relative to the source build area'
+
+  end
+
+  describe 'pre stage commands' do
+
+    it 'should run pre-prepare commands if any before build/install/source area preparation'
+    it 'should run pre-download commands if any before downloading the source archive'
+    it 'should run pre-extract commands if any before extracting the source archive'
+    it 'should run pre-configure if any before configuring the source'
+    it 'should run pre-build commands if any before building the source'
+    it 'should run pre-install commands if any before installing the source'
+    it 'should be run relative to the source build area'
+
+  end
+
+  describe 'pre stage commands' do
+
+    it 'should run post-prepare commands if any after build/install/source area preparation'
+    it 'should run post-download commands if any after downloading the source archive'
+    it 'should run post-extract commands if any after extracting the source archive'
+    it 'should run post-configure if any after configuring the source'
+    it 'should run post-build commands if any after building the source'
+    it 'should run post-install commands if any after installing the source'
+    it 'should be run relative to the source build area'
+
+  end
+
+  describe 'install sequence' do
+
+    it 'should prepare, then download, then extract, then configure, then build, then install'
+
+  end
+
+  describe 'source extraction' do
+
+    it 'should support tgz archives'
+    it 'should support tar.gz archives'
+    it 'should support tar.bz2 archives'
+    it 'should support tb2 archives'
+    it 'should support zip archives'
+
+  end
+
 end
-
-describe Sprinkle::Installers::Source, 'before installation' do
-  
-  it 'should fail if no installation area has been specified'
-  it 'should fail if no build area has been specified'
-  it 'should fail if no source download area has been specified'
-  
-end
-
-describe Sprinkle::Installers::Source, 'customized configuration' do
-  
-  it 'should override default options'
-  it 'should support specification of "enable" options'
-  it 'should support specification of "disable" options'
-  it 'should support specification of "with" options'
-  it 'should support specification of "without" options'
-  it 'should support customized build area'
-  it 'should support customized source area'
-  it 'should support customized install area'
-  
-end
-
-describe Sprinkle::Installers::Source, 'during gnu source archive style installation' do
-    
-  it 'should prepare the build area'
-  it 'should prepare the installation area'
-  it 'should prepare the source download area'
-  it 'should download the source archive'
-  it 'should extract the source archive'
-  it 'should configure the source'
-  it 'should build the source'
-  it 'should install the source'
-  
-end
-
-describe Sprinkle::Installers::Source, 'during customized installation' do
-    
-  it 'should prepare the build area'
-  it 'should prepare the installation area'
-  it 'should prepare the source download area'
-  it 'should download the source archive'
-  it 'should extract the source archive'
-  it 'should not configure the source automatically'
-  it 'should not build the source automatically'
-  it 'should install the source using a custom installation command'
-  
-end
-
-describe Sprinkle::Installers::Source, 'customized installer commands' do
-
-  it 'should be run relative to the source build area'
-  
-end
-
-describe Sprinkle::Installers::Source, 'pre stage commands' do
-
-  it 'should run pre-prepare commands if any before build/install/source area preparation'
-  it 'should run pre-download commands if any before downloading the source archive'
-  it 'should run pre-extract commands if any before extracting the source archive'
-  it 'should run pre-configure if any before configuring the source'
-  it 'should run pre-build commands if any before building the source'
-  it 'should run pre-install commands if any before installing the source'
-  it 'should be run relative to the source build area'
-  
-end
-
-describe Sprinkle::Installers::Source, 'pre stage commands' do
-
-  it 'should run post-prepare commands if any after build/install/source area preparation'
-  it 'should run post-download commands if any after downloading the source archive'
-  it 'should run post-extract commands if any after extracting the source archive'
-  it 'should run post-configure if any after configuring the source'
-  it 'should run post-build commands if any after building the source'
-  it 'should run post-install commands if any after installing the source'
-  it 'should be run relative to the source build area'
-  
-end
-
-describe Sprinkle::Installers::Source, 'install sequence' do
-  
-  it 'should prepare, then download, then extract, then configure, then build, then install'
-  
-end
-
-describe Sprinkle::Installers::Source, 'source extraction' do
-  
-  it 'should support tgz archives'
-  it 'should support tar.gz archives'
-  it 'should support tar.bz2 archives'
-  it 'should support tb2 archives'
-  it 'should support zip archives'
-  
-end
-
