2bb7cd8ecc22312e177465f0ee1af4d22a1e2e1f
Marcus Crafter <crafterm@redartisan.com>
2008-05-25 22:31:19 +1000

    Add MySQL development package requirement for Sphinx search source installer configuration

--- examples/rails/packages/search.rb
+++ examples/rails/packages/search.rb
@@ -2,4 +2,10 @@ package :sphinx, :provides => :search do
   description 'MySQL full text search engine'
   version '0.9.8-rc2'
   source "http://www.sphinxsearch.com/downloads/sphinx-#{version}.tar.gz"
+  requires :mysql_dev
+end
+
+package :mysql_dev do
+  description 'MySQL Database development package'
+  apt %w( libmysqlclient15-dev )
 end
