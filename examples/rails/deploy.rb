e119f68fa1330bf58d9fbc3356b4dfd5599fd93a
Marcus Crafter <crafterm@redartisan.com>
2008-05-26 18:44:19 +1000

    Reduce example capistrano configuration to minimum sprinkle requires

--- examples/rails/deploy.rb
+++ examples/rails/deploy.rb
@@ -1,5 +1,2 @@
-set :application, "application"
-
-role :app, "yourhost.com"
-role :web, "yourhost.com"
-role :db,  "yourhost.com", :primary => true
+set :user, 'root'
+role :app, 'yourhost.com', :primary => true
