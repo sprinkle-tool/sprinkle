ee07f7b591d6bf02aba75649e95f89673ba6b27d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2008-07-17 01:21:00 -0700

    Abstracted out aspects of installer which made it configurable to prepare for verify blocks. Specs all pass.

--- lib/sprinkle/installers/installer.rb
+++ lib/sprinkle/installers/installer.rb
@@ -1,6 +1,7 @@
 module Sprinkle
   module Installers
     class Installer
+      include Sprinkle::Runnable
       attr_accessor :delivery, :package, :options, :pre, :post
 
       def initialize(package, options = {}, &block)
@@ -20,14 +21,8 @@ module Sprinkle
         @post[stage] += commands
       end
 
-      def defaults(deployment)
-        defaults = deployment.defaults[self.class.name.split(/::/).last.downcase.to_sym]
-        self.instance_eval(&defaults) if defaults
-        @delivery = deployment.style
-      end
-
       def process(roles)
-        raise 'Unknown command delivery target' unless @delivery
+        assert_delivery
 
         if logger.debug?
           sequence = install_sequence; sequence = sequence.join('; ') if sequence.is_a? Array
@@ -38,14 +33,6 @@ module Sprinkle
           logger.info "--> Installing #{package.name} for roles: #{roles}"
           @delivery.process(@package.name, install_sequence, roles)
         end
-      end
-
-      def method_missing(sym, *args, &block)
-        unless args.empty? # mutate if not set
-          @options[sym] = *args unless @options[sym]
-        end
-
-        @options[sym] || @package.send(sym, *args, &block) # try the parents options if unknown
       end
 
       protected
---
+++ lib/sprinkle/runnable.rb
@@ -0,0 +1,21 @@
+module Sprinkle
+  module Runnable
+    def defaults(deployment)
+      defaults = deployment.defaults[self.class.name.split(/::/).last.downcase.to_sym]
+      self.instance_eval(&defaults) if defaults
+      @delivery = deployment.style
+    end
+    
+    def assert_delivery
+      raise 'Unknown command delivery target' unless @delivery
+    end
+    
+    def method_missing(sym, *args, &block)
+      unless args.empty? # mutate if not set
+        @options[sym] = *args unless @options[sym]
+      end
+
+      @options[sym] || @package.send(sym, *args, &block) # try the parents options if unknown
+    end
+  end
+end
