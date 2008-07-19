355b3aedf7c0303a5d7132ef70f70687043b11e9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2008-07-19 03:25:41 -0700

    Documented Sprinkle::Script.

--- lib/sprinkle/script.rb
+++ lib/sprinkle/script.rb
@@ -1,12 +1,22 @@
 module Sprinkle
+  # = Programmatically Run Sprinkle
+  #
+  # Sprinkle::Script gives you a way to programatically run a given
+  # sprinkle script. 
   class Script
+    # Run a given sprinkle script. This method is <b>blocking</b> so
+    # it will not return until the sprinkling is complete or fails.
+    #--
+    # FIXME: Improve documentation, possibly notify user how to tell
+    # if a sprinkling failed.
+    #++
     def self.sprinkle(script, filename = '__SCRIPT__')
       powder = new
       powder.instance_eval script, filename
       powder.sprinkle
     end
 
-    def sprinkle
+    def sprinkle #:nodoc:
       @deployment.process if @deployment
     end
   end
