4b89a0d5171ed5ae9815129568c03fa5d9641574
Marcus Crafter <crafterm@redartisan.com>
2008-08-02 20:35:33 +1000

    Add support for provisioning your local system with Sprinkle via a :local actor, added spec to validate

---
+++ lib/sprinkle/actors/local.rb
@@ -0,0 +1,26 @@
+module Sprinkle
+  module Actors
+    # = Local Delivery Method
+    #
+    # This actor implementation performs any given commands on your local system, as
+    # opposed to other implementations that generally run commands on a remote system
+    # via the network.
+    #
+    # This is useful if you'd like to use Sprinkle to provision your local machine. 
+    # To enable this actor, in your Sprinkle script specify the :local delivery mechanism. 
+    #
+    #   deployment do
+    #     delivery :local
+    #   end
+    #
+    # Note, your local machine will be assumed to be a member of all roles when applying policies
+    #
+    class Local
+      
+      def process(name, commands, roles, suppress_and_return_failures = false) #:nodoc:
+        commands.each { |command| system command }
+      end
+      
+    end
+  end
+end
---
+++ spec/sprinkle/actors/local_spec.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Sprinkle::Actors::Local do
+
+  before do
+    @local = Sprinkle::Actors::Local.new
+  end
+
+  describe 'when processing commands' do
+
+    before do
+      @commands = %w( op1 op2 )
+      @roles    = %w( app )
+      @name     = 'name'
+    
+      @local.stub!(:system).and_return
+    end
+
+    it 'should run the commands on the local system' do
+      @local.should_receive(:system).twice.and_return
+    end
+    
+    after do
+      @local.process @name, @commands, @roles
+    end
+
+  end
+
+end
