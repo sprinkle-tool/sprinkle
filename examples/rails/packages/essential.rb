fdc0de0ed1a61d8ec17d405ebbf516d0c31c5aa4
Marcus Crafter <crafterm@redartisan.com>
2008-08-02 20:36:48 +1000

    Update build essential example to update apt sources to prevent any out of date packages being used

--- examples/rails/packages/essential.rb
+++ examples/rails/packages/essential.rb
@@ -2,5 +2,8 @@
 
 package :build_essential do
   description 'Build tools'
-  apt 'build-essential'
+  apt 'build-essential' do
+    # Update the sources and upgrade the lists before we build essentials
+    pre :install, 'apt-get update'
+  end
 end
