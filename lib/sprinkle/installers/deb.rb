8a4cab4711c264596d5c9735952b50a52deadf1f
Joshua Sierles <jsierles@gmail.com>
2008-10-23 13:33:22 +0200

    add raw deb installer

---
+++ lib/sprinkle/installers/deb.rb
@@ -0,0 +1,32 @@
+module Sprinkle
+  module Installers
+    # = Deb Package Installer
+    #
+    # The Deb installer installs deb packages sourced from a remote URL
+    # 
+    # == Example Usage
+    #
+    # Installing the magic_beans deb.
+    #
+    #   package :magic_beans do
+    #     deb 'http://debs.example.com/magic_beans.deb'
+    #   end
+    #
+    class Deb < Installer
+      attr_accessor :packages #:nodoc:
+
+      def initialize(parent, packages, &block) #:nodoc:
+        super parent, &block
+        packages = [packages] unless packages.is_a? Array
+        @packages = packages
+      end
+
+      protected
+
+        def install_commands #:nodoc:
+          "wget -cq --directory-prefix=/tmp #{@packages.join(' ')}; dpkg -i #{@packages.collect{|p| "/tmp/#{p}"}.join(" ")}"
+        end
+
+    end
+  end
+end
