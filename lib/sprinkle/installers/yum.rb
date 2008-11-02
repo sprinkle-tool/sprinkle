d2f0f2e2556cdf992dd90ee48b23a0513fd91073
Marcus Crafter <crafterm@redartisan.com>
2008-11-03 08:23:01 +1100

    Add support for installing packages via yum, specs to validate

---
+++ lib/sprinkle/installers/yum.rb
@@ -0,0 +1,37 @@
+module Sprinkle
+  module Installers
+    # = Yum Package Installer
+    #
+    # The Yum package installer installs RPM packages.
+    # 
+    # == Example Usage
+    #
+    # Installing the magic_beans RPM via Yum. Its all the craze these days.
+    #
+    #   package :magic_beans do
+    #     yum 'magic_beans'
+    #   end
+    #
+    # You may also specify multiple rpms as an array:
+    #
+    #   package :magic_beans do
+    #     yum %w(magic_beans magic_sauce)
+    #   end
+    class Yum < Installer
+      attr_accessor :packages #:nodoc:
+
+      def initialize(parent, packages, &block) #:nodoc:
+        super parent, &block
+        packages = [packages] unless packages.is_a? Array
+        @packages = packages
+      end
+
+      protected
+
+        def install_commands #:nodoc:
+          "yum #{@packages.join(' ')} -y"
+        end
+
+    end
+  end
+end
---
+++ spec/sprinkle/installers/yum_spec.rb
@@ -0,0 +1,49 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Sprinkle::Installers::Yum do
+
+  before do
+    @package = mock(Sprinkle::Package, :name => 'package')
+  end
+
+  def create_rpm(rpms, &block)
+    Sprinkle::Installers::Yum.new(@package, rpms, &block)
+  end
+
+  describe 'when created' do
+
+    it 'should accept a single package to install' do
+      @installer = create_rpm 'ruby'
+      @installer.packages.should == [ 'ruby' ]
+    end
+
+    it 'should accept an array of packages to install' do
+      @installer = create_rpm %w( gcc gdb g++ )
+      @installer.packages.should == ['gcc', 'gdb', 'g++']
+    end
+
+  end
+
+  describe 'during installation' do
+
+    before do
+      @installer = create_rpm 'ruby' do
+        pre :install, 'op1'
+        post :install, 'op2'
+      end
+      @install_commands = @installer.send :install_commands
+    end
+
+    it 'should invoke the rpm installer for all specified packages' do
+      @install_commands.should =~ /yum ruby -y/
+    end
+
+    it 'should automatically insert pre/post commands for the specified package' do
+      @installer.send(:install_sequence).should == [ 'op1', 'yum ruby -y', 'op2' ]
+    end
+
+    it 'should install a specific version if defined'
+
+  end
+
+end
