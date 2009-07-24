6acd2329779deca661c73fedf07c40c4bbc2d5f7
Jan Ulbrich <jan@ulbrich-boerwang.de>
2009-07-24 12:32:28 +0200

    Additional executable verifiers checking for certain version.

--- lib/sprinkle/verifiers/executable.rb
+++ lib/sprinkle/verifiers/executable.rb
@@ -31,6 +31,23 @@ module Sprinkle
           @commands << "[ -n \"`echo \\`which #{path}\\``\" ]"
         end
       end
+
+      # Same as has_executable but with checking for e certain version number.
+      # Last option is the parameter to append for getting the version (which
+      # defaults to "-v").
+      def has_executable_with_version(path, version, get_version = '-v')
+        if path.include?('/')
+          @commands << "[ -x #{path} -a -n \"`#{path} #{get_version} 2> /dev/null | egrep -e \\\"#{version}\\\"`\" ]"
+        else
+          @commands << "[ -n \"`echo \\`which #{path}\\``\" -a -n \"`\\`which #{path}\\` #{get_version} 2>&1 | egrep -e \\\"#{version}\\\"`\" ]"
+        end
+      end
+
+      # Same as has_executable but checking output of a certain command
+      # with grep.
+      def has_version_in_grep(cmd, version)
+        @commands << "[ -n \"`#{cmd} 2> /dev/null | egrep -e \\\"#{version}\\\"`\" ]"
+      end
     end
   end
 end
