f576737458d73f32c0ef5dae95211a3a3659bcb8
Jan Ulbrich <jan@ulbrich-boerwang.de>
2009-07-25 13:05:04 +0200

    Fixed noop command adding missing file to manifest and fixing iniialize and empty command.

--- lib/sprinkle/installers/noop.rb
+++ lib/sprinkle/installers/noop.rb
@@ -5,14 +5,14 @@ module Sprinkle
     # This installer does nothing, it's simply useful for running pre / post hooks by themselves. 
     # 
     class Noop < Installer
-      def initialize(parent, &block) #:nodoc:
+      def initialize(parent, name, options, &block) #:nodoc:
         super parent, {}, &block
       end
 
       protected
 
         def install_commands #:nodoc:
-          ''
+          'echo noop'
         end
 
     end
