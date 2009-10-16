9b36f7080c611f10adaacd27f5f507f76110bad4
Oliver Kiessler <kiessler@inceedo.com>
2009-10-16 17:33:01 +0200

    Improved archive format support

--- lib/sprinkle/installers/binary.rb
+++ lib/sprinkle/installers/binary.rb
@@ -2,21 +2,44 @@ module Sprinkle
   module Installers
     # = Binary Installer
     # 
+    # binary "http://some.url.com/archive.tar.gz" do
+    #  prefix   "/home/user/local"
+    #  archives "/home/user/sources"
+    # end 
     #
     class Binary < Installer
-      def initialize(parent, source, options = {}, &block) #:nodoc:
-        @source = source
+      def initialize(parent, binary_archive, options = {}, &block) #:nodoc:
+        @binary_archive = binary_archive
+        @options = options
         super parent, options, &block
       end
 
-      def download_commands #:nodoc:
-        [ "wget -cq --directory-prefix='#{@options[:archives]}' #{@source}" ]
+      def prepare_commands #:nodoc:
+        raise 'No installation area defined' unless @options[:prefix]
+        raise 'No archive download area defined' unless @options[:archives]
+
+        [ "mkdir -p #{@options[:prefix]}",
+          "mkdir -p #{@options[:archives]}" ]
       end
 
       def install_commands #:nodoc:
-        commands = [ "bash -c 'wget -cq --directory-prefix=\'#{@options[:archives]}\' #{@source} "]
-        # TODO check archive type
-        commands << "cd #{@options[:prefix]} && tar xfvz #{@options[:archives]}/#{@source.split("/").last}'"
+        commands = [ "bash -c 'wget -cq --directory-prefix=#{@options[:archives]} #{@binary_archive}'" ]
+        commands << "bash -c 'cd #{@options[:prefix]} && #{extract_command} #{@options[:archives]}/#{@binary_archive.split("/").last}'"
+      end
+
+      def extract_command(archive_name = @binary_archive.split("/").last)
+        case archive_name
+        when /(tar.gz)|(tgz)$/
+          'tar xzf'
+        when /(tar.bz2)|(tb2)$/
+          'tar xjf'
+        when /tar$/
+          'tar xf'
+        when /zip$/
+          'unzip'
+        else
+          raise "Unknown binary archive format: #{archive_name}"
+        end
       end
     end
   end
