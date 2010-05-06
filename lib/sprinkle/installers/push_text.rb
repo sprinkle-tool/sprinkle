43ca5af752fb2367e3c56460c8a6444c0eb8a0b4
Mat Trudel <mat@geeky.net>
2010-05-06 13:51:41 -0400

    Added idempotent attribute to text appender

--- lib/sprinkle/installers/push_text.rb
+++ lib/sprinkle/installers/push_text.rb
@@ -37,7 +37,7 @@ module Sprinkle
       protected
 
         def install_commands #:nodoc:
-          "echo -e '#{@text.gsub("'", "'\\\\''").gsub("\n", '\n')}' |#{'sudo ' if option?(:sudo)}tee -a #{@path}"
+          "#{"#{'sudo ' if option?(:sudo)}grep \"^#{@text.gsub("'", "'\\\\''").gsub("\n", '\n')}$\" #{@path} ||" if option?(:idempotent) }/bin/echo -e '#{@text.gsub("'", "'\\\\''").gsub("\n", '\n')}' |#{'sudo ' if option?(:sudo)}tee -a #{@path}"
         end
 
     end
