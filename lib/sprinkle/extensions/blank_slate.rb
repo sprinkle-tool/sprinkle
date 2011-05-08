727363de10c6a63ab46155c78b06cbc2c5c1bd7c
Nick Plante <nap@zerosum.org>
2011-05-08 18:19:57 -0400

    don't undefine object_id

--- lib/sprinkle/extensions/blank_slate.rb
+++ lib/sprinkle/extensions/blank_slate.rb
@@ -1,5 +1,5 @@
 class BlankSlate #:nodoc:
-  instance_methods.each do |m| 
-    undef_method(m) unless %w( __send__ __id__ send class inspect instance_eval instance_variables ).include?(m)
+  instance_methods.each do |m|
+    undef_method(m) unless %w( __send__ __id__ send class inspect instance_eval instance_variables object_id ).include?(m.to_s)
   end
 end
