bd1803e30ca1ee9461a76e8ec6db661cab8f2314
Vladimir Malyarevich <vladimir@vladimir-note.(none)>
2011-09-15 18:15:40 +0300

    compatibility with ruby 1.9.2

--- lib/sprinkle/installers/source.rb
+++ lib/sprinkle/installers/source.rb
@@ -149,7 +149,8 @@ module Sprinkle
         # dress is overriden from the base Sprinkle::Installers::Installer class so that the command changes
         # directory to the build directory first. Also, the result of the command is logged.
         def dress(commands, stage)
-          commands.collect { |command| "bash -c 'cd #{build_dir} && #{command} >> #{@package.name}-#{stage}.log 2>&1'" }
+	  commands = [commands] if commands.is_a? String
+	  commands.map { |command| "bash -c 'cd #{build_dir} && #{command} >> #{@package.name}-#{stage}.log 2>&1'" }
         end
 
       private
