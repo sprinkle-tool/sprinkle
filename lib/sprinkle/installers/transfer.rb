sprinkle-tool/sprinkle

commit fa300fda9494ee4357b322928afbbff720a4e431

Fixing Sprinkle::Installers::Transfer pre and post hooks.

lib/sprinkle/installers/transfer.rb
@@ -135,7 +135,7 @@ module Sprinkle
 					unless pre.empty?
 						sequence = pre; sequence = sequence.join('; ') if sequence.is_a? Array
 						logger.info "#{@package.name} pre-transfer commands: #{sequence} for roles: #{roles}\n"
-						@delivery.process @package.name, sequence, roles
+						@delivery.process @package.name, [pre].flatten, roles
 					end
 					
 					recursive = @options[:recursive]
@@ -169,7 +169,7 @@ module Sprinkle
 					unless post.empty?
 						sequence = post; sequence = sequence.join('; ') if sequence.is_a? Array
 						logger.info "#{@package.name} post-transfer commands: #{sequence} for roles: #{roles}\n"
-						@delivery.process @package.name, sequence, roles
+						@delivery.process @package.name, [post].flatten, roles
 					end
         end
       end

spec/sprinkle/installers/transfer_spec.rb
@@ -42,9 +42,9 @@ describe Sprinkle::Installers::Transfer do
     end
 
 		it "should call the pre and post install commands around the file transfer" do
-			@delivery.should_receive(:process).with(@package.name, 'op1', @roles).once.ordered.and_return
+			@delivery.should_receive(:process).with(@package.name, ['op1'], @roles).once.ordered.and_return
       @delivery.should_receive(:transfer).ordered.and_return
-			@delivery.should_receive(:process).with(@package.name, 'op2', @roles).once.ordered.and_return
+			@delivery.should_receive(:process).with(@package.name, ['op2'], @roles).once.ordered.and_return
 		end	
 		
 		it "should call transfer with recursive defaulted to nil" do

commit 2c20bee62cd84ab30c250dbac45f687e80a7ef9e

Fix pre/post command handling in transfer installer broke with fa300fda9494ee4357b322928afbbff720a4e431 commit

lib/sprinkle/installers/transfer.rb
@@ -135,7 +135,7 @@ module Sprinkle
           unless pre.empty?
             sequence = pre; sequence = sequence.join('; ') if sequence.is_a? Array
             logger.info "#{@package.name} pre-transfer commands: #{sequence} for roles: #{roles}\n"
-            @delivery.process @package.name, [pre].flatten, roles
+            @delivery.process @package.name, sequence, roles
           end
 
           recursive = @options[:recursive]
@@ -169,7 +169,7 @@ module Sprinkle
           unless post.empty?
             sequence = post; sequence = sequence.join('; ') if sequence.is_a? Array
             logger.info "#{@package.name} post-transfer commands: #{sequence} for roles: #{roles}\n"
-            @delivery.process @package.name, [post].flatten, roles
+            @delivery.process @package.name, sequence, roles
           end
         end
       end