ef93c6c016a8023a1163fb652c70cafd391b9095
Peter Schrammel <peter.schrammel@gmx.de>
2011-12-07 23:29:55 +0100

    more info which command failed or succeded

--- lib/sprinkle/actors/ssh.rb
+++ lib/sprinkle/actors/ssh.rb
@@ -107,9 +107,9 @@ module Sprinkle
               channel.on_request("exit-status") do |ch, data|
                 exit_code = data.read_long
                 if exit_code == 0
-                  logger.debug(green 'success')
+                  logger.debug(green 'success "%{cmd}"' % {:cmd => cmd})
                 else
-                  logger.debug(red('failed (%d).'%exit_code))
+                  logger.debug(red('failed (%{exit_code}) "%{cmd}"'% {:exit_code=> exit_code,:cmd => cmd}))
                 end
                 res << exit_code
                 ch.close
