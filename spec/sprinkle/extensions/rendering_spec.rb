45e59d23926d5f1e9364e1785cf75eda4740c352
Koen Punt <koen@koenpunt.nl>
2013-06-21 15:32:15 +0200

    Use custom context instead of binding for templates package opts are by default available in the template as local vars added textmate properties file

--- spec/sprinkle/extensions/rendering_spec.rb
+++ spec/sprinkle/extensions/rendering_spec.rb
@@ -1,8 +1,9 @@
 require File.expand_path("../../spec_helper", File.dirname(__FILE__))
 
 describe Sprinkle::Package::Rendering, 'rendering' do
-  
+
   before do
+    @root = File.expand_path(File.join(File.dirname(__FILE__), "../.."))
     @package = package :something do
     end
   end
@@ -10,24 +11,45 @@ describe Sprinkle::Package::Rendering, 'rendering' do
   it "should be able to calculate md5s" do
     @package.md5("test").should == "098f6bcd4621d373cade4e832627b4f6"
   end
-  
+
   it "should allow passing locals to template" do
-    t=@package.template("hello <%= world %>", :world => "world")
+    t = @package.template("hello <%= @world %>", :world => "world")
     t.should == "hello world"
   end
-  
-  it "should be able to render a file from absolute path" do
-    path=File.join(__FILE__, "../../templates/test.erb")
-    t=@package.render("test")
-    t.should == "hello " 
-  end
-  
+
   it "should be able to render a file from templates" do
-    @package = package :new do
-      @world = "world"
+    Dir.chdir(@root) do
+      t = @package.render("test")
+      t.should == "hello "
     end
-    t=@package.render("test")
-    t.should == "hello world" 
   end
-    
+
+  it "should be able to render a file from absolute path" do
+    path = File.join(@root, "templates/test.erb")
+    t = @package.render(path)
+    t.should == "hello "
+  end
+  
+  it "should accept options as second argument" do
+    path = File.join(@root, "templates/test.erb")
+    t = @package.render(path, :world => "world")
+    t.should == "hello world"
+  end
+
+  it "should have the package opts available in the template" do
+    p = @package.instance :world => "earth"
+    Dir.chdir(@root) do
+      t = p.render("test")
+      t.should == "hello earth"
+    end
+  end
+
+  it "should override package opts with render opts" do
+    p = @package.instance :world => "earth"
+    Dir.chdir(@root) do
+      t = p.render("test", :world => "world")
+      t.should == "hello world"
+    end
+  end
+
 end
