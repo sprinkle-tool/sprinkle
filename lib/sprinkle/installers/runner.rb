f77cc85429326a1ff81f641cdcf2f4dc2960a153
Koen Punt <koen@koenpunt.nl>
2013-06-23 17:18:14 +0000

    added env option for runner installer
    correct to combine env variables with spaces instead of colon
    convert keys to uppercase
    
    Updated test so the order of the env var don't matter

--- lib/sprinkle/installers/runner.rb
+++ lib/sprinkle/installers/runner.rb
@@ -15,33 +15,50 @@ module Sprinkle
     #     runner [ "make world", "destroy world" ]
     #   end
     #
+    # Environment variables can be supplied throught the :env option.
+    #
+    #   package :magic_beans do
+    #     runner "make world", :env => {
+    #       :PATH => '/this/is/my/path:$PATH'
+    #     }
+    #   end
+    #
     class Runner < Installer
-      
+
       api do
         def runner(*cmds, &block)
           options = cmds.extract_options!
           install Runner.new(self, cmds, options, &block)
         end
-        
+
         # runs 'echo noop' on the remote host
         def noop
           install Runner.new(self, "echo noop")
         end
       end
-      
+
       attr_accessor :cmds #:nodoc:
       def initialize(parent, cmds, options = {}, &block) #:nodoc:
         super parent, options, &block
+        @env = options.delete(:env)
         @cmds = [*cmds].flatten
         raise "you need to specify a command" if cmds.nil?
       end
-      
+
       protected
-        
+
+        def env_str #:nodoc:
+          @env_str ||= @env.inject("env ") do |s, (k,v)|
+            s << "#{k.to_s.upcase}=#{v} "
+          end
+        end
+
         def install_commands #:nodoc:
-          sudo? ? 
-            @cmds.map { |cmd| "#{sudo_cmd}#{cmd}"} :
-            @cmds
+          cmds = @env ? @cmds.map { |cmd| "#{env_str}#{cmd}"} : @cmds
+
+          sudo? ?
+            cmds.map { |cmd| "#{sudo_cmd}#{cmd}"} :
+            cmds
         end
     end
   end
