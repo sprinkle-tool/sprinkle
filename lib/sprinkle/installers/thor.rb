4925316f0f6b7d9f780e574f775f2cd4c78032d3
Lasse Skindstad Ebert <lasseebert@gmail.com>
2013-09-27 09:44:56 +0200

    Explicit namespace for Rake

--- lib/sprinkle/installers/thor.rb
+++ lib/sprinkle/installers/thor.rb
@@ -18,7 +18,7 @@ module Sprinkle
     #   package :spec do
     #     thor 'spec', :file => "/var/setup/Thorfile"
     #   end
-    class Thor < Rake
+    class Thor < Sprinkle::Installer::Rake
       
       api do
         def thor(task, options = {}, &block)
