c11048b4c12c493094f103aef99e020bf773f6aa
Jakukyo Friel <weakish@gmail.com>
2016-05-03 22:30:41 +0800

    Use extended regex for replace_text.
    Previously it used basic regex.

--- lib/sprinkle/installers/replace_text.rb
+++ lib/sprinkle/installers/replace_text.rb
@@ -2,18 +2,20 @@ module Sprinkle
   module Installers
     # = Replace text installer
     #
-    # This installer replaces a text with another one in a file.
-    # 
+    # This installer replaces a text matching the regex  with another one in a file.
+    #
     # == Example Usage
     #
     # Change ssh port in /etc/ssh/sshd_config
     #
     #   package :magic_beans do
-    #     replace_text 'Port 22', 'Port 2500', '/etc/ssh/sshd_config'
+    #     replace_text 'Port [0-9]+', 'Port 2500', '/etc/ssh/sshd_config'
     #   end
     #
+    # Because we use sed under the hood, the regex is extended regex.
+    #
     # If you user has access to 'sudo' and theres a file that requires
-    # privileges, you can pass :sudo => true 
+    # privileges, you can pass :sudo => true
     #
     #   package :magic_beans do
     #     replace_text 'Port 22', 'Port 2500', '/etc/ssh/sshd_config', :sudo => true
@@ -25,7 +27,7 @@ module Sprinkle
     #
     class ReplaceText < Installer
       attr_accessor :regex, :text, :path #:nodoc:
-      
+
       api do
         def replace_text(regex, text, path, options={}, &block)
           install ReplaceText.new(self, regex, text, path, options, &block)
@@ -38,19 +40,19 @@ module Sprinkle
         @text = text
         @path = path
       end
-      
+
       def announce
         log "--> Replace '#{@regex}' with '#{@text}' in file #{@path}"
       end
 
       protected
-      
+
         def escape_sed_arg(s)
           escape_shell_arg(s).gsub("/", "\\\\/").gsub('&', '\\\&')
         end
-      
+
         def install_commands #:nodoc:
-          "#{sudo_cmd}sed -i 's/#{escape_sed_arg(@regex)}/#{escape_sed_arg(@text)}/g' #{@path}"
+          "#{sudo_cmd}sed -r -i 's/#{escape_sed_arg(@regex)}/#{escape_sed_arg(@text)}/g' #{@path}"
         end
 
     end
