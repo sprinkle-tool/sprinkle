1fdc120c3428547ba5447508c20d0d24b7367b62
Jakukyo Friel <weakish@gmail.com>
2016-07-11 18:47:14 +0800

    pkgin: refator to use `auto_api`.

--- lib/sprinkle/installers/pkgin.rb
+++ lib/sprinkle/installers/pkgin.rb
@@ -10,29 +10,30 @@ module Sprinkle
     #     pkgin 'magic_beans'
     #   end
     #
+    # You may also specify multiple packages as an array:
+    #
+    #   package :magic_beans do
+    #     pkgin %w(magic_beans magic_sauce)
+    #   end
+    #
     class Pkgin < PackageInstaller
 
-      attr_accessor :package_name
+      ##
+      # installs pkgsrc binary packages passed
+      # :method: pkgin
+      # :call-seq: pkgin(*packages)
+      auto_api
 
-      api do
-        def pkgin(package, &block)
-          install Pkgin.new(self, package, &block)
-        end
-      end
       verify_api do
         def has_pkgin(package)
           @commands << "pkgin list | egrep '^#{@package_name}-'"
         end
       end
-      def initialize(parent, package_name, &block) #:nodoc:
-        super parent, &block
-        @package_name = package_name
-      end
 
     protected
 
       def install_commands #:nodoc:
-        "#{sudo_cmd} /opt/pkg/bin/pkgin -y install #{@package_name}"
+        "#{sudo_cmd}pkgin -y install #{@packages.join(' ')}"
       end
     end
   end
